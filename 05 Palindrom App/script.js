73d0974d08107bdfc225ebef5df9a46977afb68b
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2022-12-20 10:39:40 +0530

    Final Code upload

--- 05 Palindrom App/script.js
+++ 05 Palindrom App/script.js
@@ -1,1 +1,19 @@
 // Palindrome Checking
+let stringToCheck = document.querySelector(`.stringToCheck`);
+let checkBtn = document.querySelector(`.checkButton`);
+
+checkBtn.addEventListener(`click`, () => {
+    console.log(stringToCheck.value);
+    let strCheck = stringToCheck.value.toString().split("").reverse().join("");
+
+    if (strCheck == stringToCheck.value.toString()) {
+        console.log("Palindrom");
+        document.querySelector(`.result`).innerHTML = "Palindrom";
+    } else {
+        document.querySelector(`.result`).innerHTML = "NOT Palindrom";
+        console.log("Not palindrom");
+    }
+
+    console.log(typeof strCheck);
+    console.log(strCheck);
+});
